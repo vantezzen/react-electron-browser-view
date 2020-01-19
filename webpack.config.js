b91b679d39f7ea945ee2bde97f8d8fad04933cdc
vantezzen <hello@vantezzen.io>
2020-01-19 14:11:07 +0100

    Remove gulp

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  
+  entry : './src/ElectronBrowserView.js',
+
+  output : {
+    path: path.join(__dirname, "lib"),
+    filename: 'ElectronBrowserView.js',
+    library: 'ElectronBrowserView',
+    libraryTarget: 'umd',
+  },
+
+  resolve: {
+    extensions: ['.js']
+  },
+
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                  "@babel/react",
+                  "@babel/preset-env"
+                ],
+            },
+        },
+    ]
+  },
+  target: 'electron-renderer',
+};
